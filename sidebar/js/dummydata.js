0c2ea50e4634a4c7797bf6dfc3379f6839598c63
AM05307 <33053586+AM05307@users.noreply.github.com>
2019-06-14 17:43:42 +0900

    본문에 표시되는 데이터

---
+++ sidebar/js/dummydata.js
@@ -0,0 +1,17 @@
+
+// 사전교육 영상 주소 
+
+// 동영상 주소 
+var src = {
+	// html
+	"HTML 1강 수업소개": "https://www.youtube.com/embed/tZooW6PritE",
+	"HTML 2강 프로젝트의 동기": "https://www.youtube.com/embed/Z7uBInm1PJY",
+	"HTML 3강 기획": "https://www.youtube.com/embed/QwCdCcsPVTQ"
+}
+
+// 페이지 header
+var dummyData = {
+	"HTML 1강 수업소개": "<div>playdata 온라인 사전교육 : html</div>",
+	"HTML 2강 프로젝트의 동기": "<div>playdata 온라인 사전교육 : html</div>",
+	"HTML 3강 기획": "<div>playdata 온라인 사전교육 : html</div>"
+}
